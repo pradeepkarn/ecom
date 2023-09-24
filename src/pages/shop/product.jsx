90a6247cd8317d5c7797905daf09e567c1904814
pradeepkarn <mail2pkarn@gmail.com>
2023-09-24 21:28:49 +0530

    before using context api state management tools

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
-const Product = () => {
-  return (
-    <div>Product</div>
-  )
+const Product = (props) => {
+    const { id, productName, price, productImage } = props.data;
+    return (
+        <div className='product'>
+            <img src={productImage} alt={productName} />
+            <div className="desciption">
+                <p>
+                    <b>{productName}</b>
+                </p>
+                <p>
+                    ${price}
+                </p>
+            </div>
+            <button className='addToCartBttn'>Add To Cart</button>
+        </div>
+    )
 }
 
 export default Product
--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Product from './product'
 import { PRODUCTS } from '../../products'
-
+import  "./shop.css";
 const Shop = () => {
     return (
         <div className='shop'>
@@ -9,9 +9,8 @@ const Shop = () => {
                 <h1>Shop</h1>
             </div>
             <div className="products">
-                {" "}
                 {PRODUCTS.map((product) => (
-                    <Product />
+                    <Product data={product} />
                 ))}
             </div>
         </div>
