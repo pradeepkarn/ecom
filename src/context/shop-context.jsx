16554a9248ba03924363ad0ed21a2a0672d49307
pradeepkarn <mail2pkarn@gmail.com>
2023-09-24 22:10:59 +0530

    reached at add to cart logic

---
+++ src/context/shop-context.jsx
@@ -0,0 +1,28 @@
+import React, { createContext, useState } from 'react'
+import { PRODUCTS } from '../products'
+export const ShopContext = createContext(null)
+export const ShopContextProvider = (props) => {
+
+    const getDefaultCart = () => {
+        let cart = {}
+        for (let i = 1; i < PRODUCTS.length; i++) {
+            cart[i] = 0
+        }
+        return cart;
+    }
+
+    const [cartItems, setCartItems] = useState(getDefaultCart());
+    const addToCart = (itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
+    }
+    const removeFromCart = (itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
+    }
+    const contextValue = {cartItems, addToCart, removeFromCart}
+    console.log(cartItems)
+    return (
+        <ShopContext.Provider value={contextValue}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+}
--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../../context/shop-context';
 
 const Product = (props) => {
     const { id, productName, price, productImage } = props.data;
+    const { addToCart, cartItems } = useContext(ShopContext)
+    const cartItemAmount = cartItems[id]
     return (
         <div className='product'>
             <img src={productImage} alt={productName} />
@@ -13,7 +16,9 @@ const Product = (props) => {
                     ${price}
                 </p>
             </div>
-            <button className='addToCartBttn'>Add To Cart</button>
+            <button onClick={() => addToCart(id)} className='addToCartBttn'>
+                Add To Cart {cartItemAmount>0 && <>({cartItemAmount})</>}
+            </button>
         </div>
     )
 }
